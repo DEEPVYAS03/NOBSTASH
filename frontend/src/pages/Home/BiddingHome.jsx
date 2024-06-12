5261f75d3eb5ce0658a4bedb063a2cb2a9850ae5
Deep Vyas <deepvyas2003@gmail.com>
2024-06-12 16:07:12 +0530

    Fixed home

--- frontend/src/pages/Home/BiddingHome.jsx
+++ frontend/src/pages/Home/BiddingHome.jsx
@@ -10,12 +10,10 @@ const BiddingHome = () => {
 
   const toggleDiv = () => {
     setIsOpen(!isOpen);
-   
   };
 
   const toggleTopBidders = () => {
     setIsTopBiddersOpen(!isTopBiddersOpen);
-    
   };
 
   const toggleBid = () => {
@@ -30,25 +28,37 @@ const BiddingHome = () => {
     }
   }, [isBidOpen, isTopBiddersOpen, isOpen]);
 
-
   return (
-    <div
-      className={`flex flex-col mt-4 mb-10 sm:flex-row sm:pt-2 sm:mb-0 sm:mt-2 sm:p-4`}
-    >
+    <div className="flex flex-col mt-4 mb-10 sm:flex-row sm:pt-2 sm:mb-0 sm:mt-2 sm:p-4">
       {/* medium size */}
       <div
-        className={`${isAnyOpen ? "flex flex-row ":" "} sm:flex-col sm:w-1/2 sm:items-center sm:mb-0 sm:justify-normal`}
+        className={`${
+          isAnyOpen ? "flex flex-row " : " "
+        } sm:flex-col sm:w-1/2 sm:items-center sm:mb-0 sm:justify-normal`}
       >
         {/* Image */}
-        <div className={`sm:w-10/12 lg:w-9/12 ${isAnyOpen?"w-[12rem] h-[12rem] mx-auto mr-0":"mx-auto"}  w-9/12  h-[25rem] sm:mx-auto mb-2 sm:w-10/12 sm:h-[22rem] sm:mb-2 `}>
+        <div
+          className={`sm:w-10/12 lg:w-9/12 ${
+            isAnyOpen ? "w-[12rem] h-[12rem] mx-auto mr-0" : "mx-auto"
+          } w-9/12 ${
+            isAnyOpen ? "h-48" : "h-[25rem]"
+          } sm:h-[22rem] sm:mb-2 sm:mx-auto`}
+        >
           <img
             src={painting}
             alt="Oil Painting"
             className="w-full h-full object-fill rounded-md shadow-md"
           />
         </div>
+
         {/* Description */}
-        <div className={`sm:w-10/12 lg:w-9/12 h-fit ${isAnyOpen ? "w-[18rem] h-[12rem] mx-auto ml-2" : "w-9/12 mx-auto"}  px-8 py-4  bg-white sm:mx-auto rounded-lg shadow-lg border-gray-200 border-2 mb-2`}>
+        <div
+          className={`sm:w-10/12 lg:w-9/12 h-fit ${
+            isAnyOpen ? "w-[18rem] h-[12rem] mx-auto ml-2" : "w-9/12 mx-auto"
+          } ${
+            isAnyOpen ? "h-48" : "h-auto"
+          } px-8 py-4 bg-white sm:mx-auto rounded-lg shadow-lg border-gray-200 border-2 mb-2`}
+        >
           <h2 className="text-lg  font-bold">Oil Painting by Famous Artist</h2>
           <p className="=sm:mb-0">It's a picture of a person</p>
           <p className="font-semibold sm:mb-0">Auction ID - xxxxxx</p>
@@ -57,7 +67,7 @@ const BiddingHome = () => {
         </div>
 
         {/* More Information */}
-        <div className="border rounded mx-auto sm:w-10/12 lg:w-9/12 shadow-md mt-2  hidden sm:block sm:mt-1 sm:mb-6">
+        <div className="border rounded mx-auto sm:w-10/12 lg:w-9/12 shadow-md mt-2 hidden sm:block sm:mt-1 sm:mb-6">
           <button
             className="px-4 py-2 rounded shadow-md w-full flex justify-between"
             onClick={toggleDiv}
@@ -84,7 +94,11 @@ const BiddingHome = () => {
       <div className="sm:w-1/2 lg:5/12 sm:flex sm:flex-col sm:items-center">
         {/* Top Bidders */}
 
-        <div className={`${isAnyOpen ? "w-[30rem]" : "w-9/12"} mx-auto p-2 px-4 border-gray-200 border-2 bg-white rounded shadow-md sm:w-11/12 lg:w-10/12`}>
+        <div
+          className={`${
+            isAnyOpen ? "w-[30rem]" : "w-9/12"
+          } mx-auto p-2 px-4 border-gray-200 border-2 bg-white rounded shadow-md sm:w-11/12 lg:w-10/12`}
+        >
           <div
             className="flex justify-between items-center w-full mb-1 cursor-pointer"
             onClick={toggleTopBidders}
@@ -179,7 +193,11 @@ const BiddingHome = () => {
         </div>
 
         {/* Bid Section */}
-        <div className={`${isAnyOpen ? "w-[30rem]" : "w-9/12"} mx-auto mt-2 p-2 px-4 border-gray-200 border-2 bg-white rounded shadow-md sm:mt-4 sm:w-11/12 lg:w-10/12`}>
+        <div
+          className={`${
+            isAnyOpen ? "w-[30rem]" : "w-9/12"
+          } mx-auto mt-2 p-2 px-4 border-gray-200 border-2 bg-white rounded shadow-md sm:mt-4 sm:w-11/12 lg:w-10/12`}
+        >
           <div
             className="flex justify-between items-center w-full cursor-pointer"
             onClick={toggleBid}
@@ -243,7 +261,11 @@ const BiddingHome = () => {
         </div>
 
         {/* More Information */}
-        <div className={`${isAnyOpen ? "w-[30rem]" : "w-9/12"} border rounded mx-auto shadow-md mt-2 sm:hidden`}>
+        <div
+          className={`${
+            isAnyOpen ? "w-[30rem]" : "w-9/12"
+          } border rounded mx-auto shadow-md mt-2 sm:hidden`}
+        >
           <button
             className="px-4 py-2 rounded shadow-md w-full flex justify-between"
             onClick={toggleDiv}
